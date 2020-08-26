738dd397e3d94efcd88203424139f3da84310317
Pablo Pioli <15127047+pablopioli@users.noreply.github.com>
2020-08-26 00:25:40 -0300

    Add non generic version of RequestResult

--- src/RequestResult.cs
+++ src/RequestResult.cs
@@ -1,5 +1,27 @@
 ﻿namespace Boring
 {
+    public class RequestResult
+    {
+        public bool IsFailure => TextError.IsNotNullOrEmpty() || HttpError != null;
+        public bool IsHttpError => HttpError != null;
+
+        public string TextError { get; set; }
+        public HttpError HttpError { get; set; }
+
+        public RequestResult()
+        { }
+
+        public RequestResult(string textError)
+        {
+            TextError = textError;
+        }
+
+        public RequestResult(HttpError httpError)
+        {
+            HttpError = httpError;
+        }
+    }
+
     public class RequestResult<T>
     {
         public bool IsFailure => TextError.IsNotNullOrEmpty() || HttpError != null;
