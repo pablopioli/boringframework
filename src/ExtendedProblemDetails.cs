764c3365d7efe7799c7f590268ac0f02deb34d33
Pablo Pioli <15127047+pablopioli@users.noreply.github.com>
2020-12-10 16:23:05 -0300

    Adds ExtendedProblemDetails

---
+++ src/ExtendedProblemDetails.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace Boring
+{
+    public class ExtendedProblemDetails
+    {
+        public string Detail { get; set; }
+        public int? Status { get; set; }
+        public string Title { get; set; }
+        public string Type { get; set; }
+        public Dictionary<string, string[]> Errors { get; set; } = new Dictionary<string, string[]>();
+
+        public string GetErrorMessage()
+        {
+            var result = Title;
+
+            if (Errors.Count > 0)
+            {
+                var sb = new StringBuilder();
+                foreach (var error in Errors)
+                {
+                    if (sb.Length > 0)
+                    {
+                        sb.Append(" - ");
+                    }
+                    sb.Append(string.Join(" - ", error.Value));
+                }
+
+                result = sb.ToString();
+            }
+
+            return result;
+        }
+    }
+}
